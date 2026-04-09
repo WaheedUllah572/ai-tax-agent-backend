e9c0f569d652b73343a24dc7df52369290c88c48
Waheed <your-email@example.com>
2026-04-09 21:59:11 +0500

    fix: cors properly

--- main.py
+++ main.py
@@ -33,7 +33,9 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "*"
+        "http://localhost:3000",
+        "https://ai-tax-agent-frontend.vercel.app",
+        "https://ai-tax-agent-frontend-atxedghz7-waheed-ullahs-projects-0b4cca03.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
